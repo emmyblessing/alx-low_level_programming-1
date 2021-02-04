a1045172f03ed46a31637200aa97115807c66a70
RuthNjeri6 <davidmbuvi@icloud.com>
2021-02-04 22:22:14 +0300

    Low_level programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,11 @@
  */
 int main(void)
 {
-int n;
-n = 97;
-while (n <= 122)
-{
-putchar(n);
-n++;
-}
+int num;
+num = 97;
+while (num <= 122)
+putchar(num);
+num++;
 putchar('\n');
 return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,9 @@ int main(void)
 int m, n;
 
 for (m = 97; m <= 122; m++)
-{
 putchar(m);
-}
 for (n = 65; n <= 90; n++)
-{
 putchar(n);
-}
 putchar('\n');
 return (0);
 }
