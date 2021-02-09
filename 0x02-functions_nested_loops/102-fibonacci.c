9b5f518dd451fa95e891b5cbcd18747a6fe50009
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-09 22:34:42 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,16 +10,16 @@
 int main(void)
 {
 	unsigned long int i;
-	unsigned long int before = 1;
-	unsigned long int after = 2;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
 
-	printf("%lu", before);
+	printf("%lu", bef);
 
 	for (i = 1; i < 50; i++)
 	{
-		printf(", %lu", after);
-		after += before;
-		before = after - before;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
 	printf("\n");
 	return (0);
