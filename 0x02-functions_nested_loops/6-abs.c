6af1364a06df469b6adc692d890401fb296a97a8
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-10 23:10:42 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _abs - returns absolute value of an int
+ * @n: integer from which to get an absolute value
+ * Return: Void.
  */
-int _abs(int s){
- if(s<0){
-     return (s*-1);
- }else{
-     return(s);
- }
-return 0;
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
 }
