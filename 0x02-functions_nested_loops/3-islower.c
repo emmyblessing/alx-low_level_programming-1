eb9981f6af4b23a5502affde43e316d24791e82b
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-10 22:50:35 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ *_islower - checks for a lowercase character
+ *@c: letter being tested
+ * Return: Always 0
  */
-int _islower(int c){
-   if(c >=97 && c <= 122 ){
-       return 1;
-   }else{
-       return 0;
-   }
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
