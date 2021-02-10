9dfe2e485f0601c6d7cd509432fbc4f87594feb9
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-10 21:50:19 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-char ch[10] = "Holberton";
-int i = 0;
-while (i < 9)
-{
-_putchar(ch[i]);
-i++;
-}
-_putchar('\n');
-return (0);
+/** * main - print Holberton from function _putchar * Return: 0 */ 
+int _putchar(char c); 
+int main(void){
+  _putchar('H'); 
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t'); 
+  _putchar('o');
+  _putchar('n');
+  _putchar('\n');
+   return (0);
 }
