e724e3a147c2f8d3a0edfe5d58905eaa2f4af2bb
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-11 23:47:47 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,14 @@
-#include"holberton.h" 
+#include "holberton.h"
+#include <stdio.h>
+
 /**
  * print_line - Print line dependent on the integer n.
  * @n : The number of '_' characters to use
  * Return: Void.
  */
-void print_line(int n){
-
-    int i;
+void print_line(int n)
+{
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
