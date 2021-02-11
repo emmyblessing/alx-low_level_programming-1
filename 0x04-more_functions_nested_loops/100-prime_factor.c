3b1b38327df3404837e6ff5d3888eec912356550
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-11 23:55:02 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,15 +10,15 @@
 int main(void)
 {
 	long int x = 612852475143;
-	long int y;
+	long int py;
 
-	for (y = 2; y < x; y++)
+	for (py = 2; py < x; py++)
 	{
-		if (x % y == 0)
+		if (x % py == 0)
 		{
-			x = x / y;
+			x = x / py;
 		}
 	}
-	printf("%ld\n", y);
+	printf("%ld\n", py);
 	return (0);
 }
