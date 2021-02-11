a71889af830d622ed8e4b2a38f2447f0cd1efe42
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-12 00:07:40 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - func
  *
- * Return: Always 0.
+ * Return - 0
  */
-void print_alphabet(){
-    int j=0,i;
-    while(j<10){
-        for (i=97;i<=122;i++){
-        _putchar(i);
-    }
-     _putchar('\n');
-     j++;
-
-    }
-    
+void print_alphabet_x10(void)
+{
+int b = 0;
+while (b < 10)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+b++;
+}
 }
