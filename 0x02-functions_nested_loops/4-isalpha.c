aef2d7d54e3c8ad4e212f8b9b50920f8d24744a8
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-12 00:09:47 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,23 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _isalpha - check the code for Holberton School students.
+ *
+ * @c: is c an integer argument
  *
  * Return: Always 0.
  */
-int _isalpha(int c){
-   if((c >=97 && c <= 122) || (c >=65 && c <= 90) ){
-       return 1;
-   }else{
-       return 0;
-   }
+
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
