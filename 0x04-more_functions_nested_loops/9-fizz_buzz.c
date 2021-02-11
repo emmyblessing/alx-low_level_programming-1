4f6046b0e255efcb3d2412348c512aa02bbf4f47
David Mbuvi Musau <78023856+davidmusau@users.noreply.github.com>
2021-02-11 23:33:05 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,13 @@ void fizz_buzz(void){
 
     for(i=1; i<=100; i++){
         if(i%3 ==0 && i%5 == 0){
-            printf("FizzBuzz ");
+            printf("%s", "FizzBuzz");
         }else if (i%3 == 0)
         {
-            printf("Fizz ");
+            printf("%s","Fizz ");
         }else if (i%5 ==0)
         {
-            printf("Buzz ");
+            printf("%s","Buzz ");
         }else
         {
             printf("%d ",i);
