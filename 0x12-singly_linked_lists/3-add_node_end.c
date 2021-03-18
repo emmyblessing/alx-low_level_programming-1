e8453f733da289221d60bbafbc055c36ab335abd
African Coder <78023856+davidmusau@users.noreply.github.com>
2021-03-18 20:49:56 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,31 +2,36 @@
 /**add_node_end - adds a new node at the end of a linked list @head: double pointer to the list_t list,@str: string to put in the new node Return: address of the new element or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str) {
-	list_t *new;
-	list_t *temp = *head;
-	unsigned int len = 0;
+	list_t *temp, *temp2;
+	unsigned int length = 0;
 
-	while (str[len])
-		len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
+	if (str == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = NULL;
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new;
-		return (new);
+		*head = temp;
+		return (temp);
 	}
 
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
-
-	return (new);
+	temp2 = *head;
+	while (temp2->next)
+		temp2 = temp2->next;
+	temp2->next = temp;
+	return (temp);
 }
