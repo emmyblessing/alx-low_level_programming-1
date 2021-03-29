7ad188210853d112d4aaadff346b5ff37045ccb6
David Mbuvi <davidmbuvi@icloud.com>
2021-03-29 22:39:15 +0300

    Alx-Low_level programming

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,32 +9,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd,i,y;
+	ssize_t k;
+	int fd;
 	char *buf;
 
 	if (!filename)
 		return (0);
+		buf = malloc(sizeof(char) * letters+1);
+	if (buf == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
+	    free(buf);
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+	k = read(fd, buf, sizeof(char) * letters);
+	if (k ==-1)
 	{
 		free(buf);
+		close(fd);
 		return (0);
 	}
-	buf[i] = '\0';
-
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
+	k = write(STDOUT_FILENO, buf, k);
+	if (k == -1)
 	{
 		free(buf);
+		close(fd);
 		return (0);
 	}
 	free(buf);
-	return (y);
+	close(fd);
+	return (k);
 }
