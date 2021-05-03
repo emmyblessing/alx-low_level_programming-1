3d420233d3a43a7ef7e9f0048bef3aacf441fbef
David Mbuvi <davidmbuvi@icloud.com>
2021-05-03 23:15:06 +0300

    track 0

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ wget https://raw.githubusercontent.com/davidmusau
-/
-alx-low_level_programming/master/0x18-dynamic_libraries/mao.so
-export LD_PRELOAD=/tmp/mao.so
+wget -P .. https://raw.githubusercontent.com/davidmusau/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
